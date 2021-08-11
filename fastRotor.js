8e59dcd14a0240b2488d1066b6178299c80c76aa
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2021-08-10 21:13:27 -0300

    Começo da programação do Fast Rotor

--- fastRotor.js
+++ fastRotor.js
@@ -1,1 +1,30 @@
-const disc = require('./discs.js')
+// const disc = require('./discs.js')
+const alphabet = 'abcdefghijklmnopqrstuvwxyz '
+//const disc = require('./discs.js')
+
+const fastRotor = function (number,message){
+    let disc = ''
+    let encrypted = ''
+
+    if (number === 'I'){
+        disc = 'EKMFLGDQVZNTOWYHXUSPAIBRCJ '
+    }
+    else if (number ==='II'){
+        disc = 'AJDKSIRUXBLHWTMCQGZNPYFVOE '
+    }
+    else if (number === 'III'){
+        disc = 'BDFHJLCPRTXVZNYEIWGAKMUSQO '
+    }
+    else if (number === 'IV'){
+        disc = 'ESOVPZJAYQUIRHXLNFTGKDCMWB '
+    }
+    else if (number === 'V'){
+        disc = 'VZBRGITYUPSDNHLXAWMJQOFECK '
+    }
+
+    for (let i = 0; i < message.length; i++){
+        encrypted += disc[alphabet.indexOf(message[i])]
+    }
+    return encrypted
+}
+console.log(fastRotor('I','ola meu nome e leonardo'))
