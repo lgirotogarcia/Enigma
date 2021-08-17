95898557bba1b83f37b4d9883da404ee70d68a9c
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2021-08-17 15:51:46 -0300

    Alteração no tipo do array de discs.js, transformando-o em um array 2D. Alteração na função na função do plugBoard.js para que inverta 2 caracteres de posição no disco escolhido.

--- plugBoard.js
+++ plugBoard.js
@@ -1,18 +1,19 @@
 const discs = require("./discs")
 const text = require("./text.js")
-/* slice para remover as letras das posições originais, e após isso, invertê-las de posição, e usar o slice novamente para adiciona-las ao local*/
-const trocarLetra = function (number,letter1,letter2){
-    // let letra1 = disc[number].indexOf(letter1)
-    // let letra2 = disc[number].indexOf(letter2)
-    // let letra3 = letra1
 
-    // letra1 = letra2
-    // letra2 = letra3
-    // return disc
 
-    const disc = discs[number]
-    disc.slice(discs[number].indexOf(letter1),1)
-    return disc
+const inverterTexto = function (number, letter1, letter2){
+    const alphabet = discs
+
+    const letra1 = alphabet[number].indexOf(letter1)
+    const letra2 = alphabet[number].indexOf(letter2)
+
+    alphabet[number].splice(letra1,1,letter2)
+    alphabet[number].splice(letra2,1,letter1)    
+
+    return alphabet[number]
 }
 
-console.log(trocarLetra(0,'a','b'))
+console.log(inverterTexto(0,'A','B'))
+
+
