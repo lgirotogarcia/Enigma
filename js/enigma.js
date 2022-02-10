0e9f9c564cb27902bf9d0619f98b13241fdd8e6d
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2022-02-10 12:48:04 -0300

    Alteração semântica feita em enigma.js, e readição momentânea dos espaços nos alfabetos

--- js/enigma.js
+++ js/enigma.js
@@ -5,12 +5,12 @@ const decrypt = require('./decrypt.js');
 
 
 const codificar = function(fast, middle, slow, code){
-    return encrypt(fast,(encrypt(middle,(encrypt(slow,encrypt(slow, encrypt(middle,encrypt(fast,text(code)))))))));
+    return encrypt(fast,encrypt(middle,encrypt(slow,encrypt(slow, encrypt(middle,encrypt(fast,text(code)))))));
 }
 console.log(codificar(1, 5, 4, 'Qualquer coisa'));
 
 const decodificar = function(fast, middle, slow, code){
-    return decrypt(fast,(decrypt(middle,(decrypt(slow,decrypt(slow, decrypt(middle,decrypt(fast,text(code)))))))));
+    return decrypt(fast,decrypt(middle,decrypt(slow,decrypt(slow, decrypt(middle,decrypt(fast,text(code)))))));
 }
 console.log(decodificar(1, 5, 4, 'FYEQFYUC ILWVE'));
 
