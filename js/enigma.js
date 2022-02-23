1d2b46a9f03600aa101463091126e64b987690f2
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2022-02-23 09:55:39 -0300

    Pequena alteração na função code em enigma.js

--- js/enigma.js
+++ js/enigma.js
@@ -2,23 +2,24 @@ const encrypt = require('./encrypt.js');
 const text = require('./text.js');
 const decrypt = require('./decrypt.js');
 
-const decrypto = function (fast, middle, slow, code) {
-    
-    let decrypting = decrypt(fast,decrypt(middle,decrypt(slow,decrypt(slow,decrypt(middle,decrypt(fast,code))))));
-    
-    return decrypting;
-}
 
 
 const code = function(fast, middle, slow, code){
     
-
+    
     const encrypto = function (fast, middle, slow, code) {
         
         let encryptRotor = encrypt(fast,encrypt(middle,encrypt(slow,encrypt(slow,encrypt(middle,encrypt(fast,code))))));        
         
         return encryptRotor.toString();
     };
+    
+    const decrypto = function (fast, middle, slow, code) {
+        
+        let decrypting = decrypt(fast,decrypt(middle,decrypt(slow,decrypt(slow,decrypt(middle,decrypt(fast,code))))));
+        
+        return decrypting;
+    }
     
     const encrypted = encrypto(fast, middle, slow, code)
     const decrypted = decrypto(fast, middle,slow, encrypted)
